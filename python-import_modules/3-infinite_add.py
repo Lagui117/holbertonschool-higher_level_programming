710ada282ba24746b4266178008aa313fcc4869d
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-05 18:48:42 +0200

    python-import_modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
-number_of_arguments = len(sys.argv)
-total_of_arguments = 0
+if __name__ == "__main__":
+    import sys
 
-i = 1
-while i < len(sys.argv):
-    int(sys.argv[i])
-    total_of_arguments += int(sys.argv[i])
-    i += 1
-print("{}".format(total_of_arguments))
+    total_of_arguments = 0
+    i = 1
+    while i < len(sys.argv):
+        total_of_arguments += int(sys.argv[i])
+        i += 1
+    print("{}".format(total_of_arguments))
+
