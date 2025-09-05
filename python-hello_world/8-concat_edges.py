9133e93ea98e9b6d9396ce7d58d1f27eb6d0ff81
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-05 21:54:03 +0200

    Add 8-concat_edges.py: create new sentence from slices

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
-str = str[39:67] + str[107:112] + str[0:6]
+str = "Python is an object-oriented programming language"
+print(str[15:39] + str[39:53])
