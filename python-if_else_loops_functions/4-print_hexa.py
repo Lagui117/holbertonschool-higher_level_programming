07b97216b238ed3630f4c896edf22ce23535afc6
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-05 21:59:47 +0200

    Add 4-print_hexa.py: decimal and hexadecimal printing

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-i = 0
-while i < 99:
-    nombre_hexadecimal = hex(i)
-    print("{}={}".format(i, nombre_hexadecimal))
-    i += 1
+for i in range(99):
+    print("{} = {}".format(i, hex(i)))
