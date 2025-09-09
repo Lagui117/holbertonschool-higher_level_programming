d2ffa5f187bbf20d5bcb32349ace2104c3b63f76
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-09 14:28:19 +0200

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = len(sentence),sentence[0]
-    for i in len(sentence):
     if sentence == "":
-        return None 
-    if sentence != "":
-        return sentence[0]
-    else :
-        return tuple_a
-    
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
