2a7a3afe493ea9165c905bff624d2b8de032546e
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-09 13:52:42 +0200

    8-multiple_returns.py

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tuple_a = len(sentence),sentence[0]
+    for i in len(sentence):
+    if sentence == "":
+        return None 
+    if sentence != "":
+        return sentence[0]
+    else :
+        return tuple_a
+    
