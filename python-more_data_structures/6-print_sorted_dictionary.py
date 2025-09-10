2708b517ab01e5554ac611540a7ecf6438b3e2c8
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-10 10:27:11 +0200

    6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     new_a_dictionary = {}
     for i in a_dictionary:
         sorted(a_dictionary)
-        new_a_dictionnary = a_dictionary
+        a_dictionary = new_a_dictionary
         return new_a_dictionary
