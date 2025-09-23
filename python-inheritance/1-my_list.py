d307db43577e30845df946ac209df6bed2c39ee3
Lagui117 <guillaumewatelet06@gmail.com>
2025-09-23 15:57:30 +0200

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Custom list subclass with a method to print a sorted view."""
 
-
 class MyList(list):
     """List subclass that can print itself sorted without modifying content."""
 
     def print_sorted(self):
         """Print the list items in ascending order without in-place changes."""
-        print (sorted(self))
+        print(sorted(self))
